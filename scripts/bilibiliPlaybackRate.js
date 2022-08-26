eb9efc3a0c7febaa93434f68a3aed1830f150626
ChenXiao Liu <810196813@qq.com>
2022-08-26 16:18:35 -0500

    setup

---
+++ scripts/bilibiliPlaybackRate.js
@@ -0,0 +1,21 @@
+{
+  const createPlaybackRateList = (...rates) => {
+    const result = []
+
+    rates.forEach(rate => {
+      const li = document.createElement('li')
+      li.className = 'bpx-player-ctrl-playbackrate-menu-item'
+      li.setAttribute('data-value', rate)
+      li.textContent = `${rate}x`
+      result.push(li)
+    })
+    return result
+  }
+  const item = document.getElementsByClassName('bpx-player-ctrl-playbackrate-menu-item')
+  const interval = setInterval(() => {
+    if (item[0]) {
+      item[0].after(...createPlaybackRateList(1.9, 1.8, 1.7, 1.6))
+      clearInterval(interval)
+    }
+  }, 1000)
+}
