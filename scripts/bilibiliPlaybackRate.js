cbdd552dfbd39efbde6ec3cc4f1174fc7619d3a8
ChenXiao Liu <810196813@qq.com>
2023-04-17 12:03:22 +0800

    add more play back rate

--- scripts/bilibiliPlaybackRate.js
+++ scripts/bilibiliPlaybackRate.js
@@ -15,6 +15,7 @@
   const interval = setInterval(() => {
     if (item[0]) {
       item[0].after(...createPlaybackRateList(1.9, 1.8, 1.7, 1.6))
+      item[0].before(...createPlaybackRateList(4.0, 3.0))
       clearInterval(interval)
     }
   }, 1000)
